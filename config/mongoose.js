4982e22db47701db766e526332a15ff2ffd2f937
Deepika <121260201+Deepika1252001@users.noreply.github.com>
2023-04-29 09:48:14 +0530

    Update mongoose.js
    updated mongoodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -21,7 +21,7 @@ const mongoose = require("mongoose");
 
 // const env = require('./environment');
 
-mongoose.connect('mongodb://127.0.0.1:27017/');
+mongoose.connect('mongodb+srv://paldeepika12501:CtvZqJrPUEEz5ALm@cluster0.mjdyxqz.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
